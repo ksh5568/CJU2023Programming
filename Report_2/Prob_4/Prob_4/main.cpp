2e60f0666c1faa9fd7134a6d3e6ffb5bb922d81f
ksh5568 <144317349+ksh5568@users.noreply.github.com>
2023-10-21 16:23:00 +0900

    Update main.cpp

--- Report_2/Prob_4/Prob_4/main.cpp
+++ Report_2/Prob_4/Prob_4/main.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define G 9.81  // 중력가속도 (단위 : m/s^2)
-#define H 100.00  // 높이 변위 (단위 : m)
+#define H 100.00  // 초기 높이 (단위 : m)
 #define KG 1  // 케틀벨 무게 (단위 : kg)
 
 int main(void)
 {
 	double nTIME = 0.0000;  // 시간에 대한 변수 설정
-	double height = 0.0000;  // 초기 높이에 대한 변수 설정
+	double nLAND = 0.0000;  //  지면 대한 변수 설정
 
 	printf("This is a program that calculates free fall motion.\n");  // 자유낙하 운동에 대한 프로그램이라는 것을 출력
 	printf("When Youngsu lets a 1kg kettlebell fall freely from a height of 100m,\n");  // 1kg 케틀벨이 100m 높이에서 떨어질 때를 출력
@@ -14,10 +14,10 @@ int main(void)
 
 	do
 	{
-		height = 0.5 * G * (nTIME * nTIME);  // 자유낙하운동 공식
+		nLAND = H - 0.5 * G * (nTIME * nTIME);  // 자유낙하운동 공식
 		nTIME = nTIME + 0.0001;  // 반복 될때 0.0001s 씩 증가
 
-	} while (height < H);  // 높이 변위보다 작을 시 계속하여 연산
+	} while (nLAND > 0.0);  // 지면보다 높으면 계속하여 연산
 	
 	printf("\n");
 	printf("Time : %.3lf s\n", nTIME);  // 결과값 출력
