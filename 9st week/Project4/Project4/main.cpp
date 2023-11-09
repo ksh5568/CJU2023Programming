e072ab8b6133437faa3b10d644897c55cb35b418
ksh5568 <144317349+ksh5568@users.noreply.github.com>
2023-11-09 17:34:53 +0900

    Update main.cpp

--- 9st week/Project4/Project4/main.cpp
+++ 9st week/Project4/Project4/main.cpp
@@ -3,9 +3,17 @@
 
 int main(void)
 {
+	// 각 char형 배열 초기화
+	char str1[80] = { 'p', 'i', 'g' }; 
+	char str2[] = "elephant";
 	char str3[5] = "apple";
+	char str4[2] = { "sun","moon" };
 
+	// 각 char형 배열 출력
+	printf("%s\n", str1);
+	printf("%s\n", str2);
 	printf("%s\n", str3);
+	printf("%s\n", str4);
 
 	return 0;
 }
