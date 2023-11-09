a3e5473ef54c707e4a2fb12cdc17cdeeed086e1b
ksh5568 <ksh26645568@gmail.com>
2023-11-09 16:39:26 +0900

    project1
    project1

---
+++ 9st week/Project1/Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define THREE 3;
+#define FIVE 5;
+#define TEN 10;
+
+int main(void)
+{
+	int nA[5];  // 정수 5개를 저장할 배열
+	int nB[3];  // 실수 10개를 저장할 배열
+	double nC[10];  // 배열 요소 개수가 3개인 int형 배열
+	char nD[5];  // 첨자의 최댓값이 4인 char형 배열
+
+	return 0;
+}
